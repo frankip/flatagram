f4a24be56668a42b8e4b09a93c9cea482d13f9b4
Francis <gwadafrank@gmail.com>
2022-09-07 20:13:23 +0300

    fetch image and update dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 // write your code here
 const baseUrl = "http://localhost:3000";
+// const imageCard = document.querySelector('.image-card')
+
 const init = () => {
     console.log("we are");
 
-    fetchDog().catch(e => console.log(e))
+    fetchDogData().catch((e) => console.log(e));
 }
 
-async function fetchDog(){
-    const response = await fetch(`${baseUrl}/images/1`);
+async function fetchDogData(){
+    const imageResponse = await fetch(`${baseUrl}/images/1`);
     const data = await response.json()
 
-    console.log(data);
+    handleUpdateCard(data)
+    // handleComments(data)
+
+    // console.log(imageCard);
+
+}
+
+const handleUpdateCard = (data) => {
+    imageCard.querySelector('#card-title').textContent = data.title
+    imageCard.querySelector('#card-image').src = data.image
+    imageCard.querySelector("#like-count").textContent = data.likes;
+
+
     
 }
+
+// const handleComments = (data)=> {
+//     const comments = imageCard.querySelector("#comments-list");
+
+//     console.log(data);
+// }
 
 document.addEventListener('DOMContentLoaded', init)
