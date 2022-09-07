814f86925b37b6329944ff96bf560152dc4ff26a
Francis <gwadafrank@gmail.com>
2022-09-07 20:32:21 +0300

    fetch comments and update dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // write your code here
 const baseUrl = "http://localhost:3000";
-// const imageCard = document.querySelector('.image-card')
+const imageCard = document.querySelector('.image-card')
 
 const init = () => {
     console.log("we are");
@@ -10,12 +10,12 @@ const init = () => {
 
 async function fetchDogData(){
     const imageResponse = await fetch(`${baseUrl}/images/1`);
-    const data = await response.json()
+    const commentsResponse = await fetch(`${baseUrl}/comments`)
+    const data = await imageResponse.json();
+    const comments = await commentsResponse.json();
 
     handleUpdateCard(data)
-    // handleComments(data)
-
-    // console.log(imageCard);
+    handleComments(comments);
 
 }
 
@@ -28,10 +28,17 @@ const handleUpdateCard = (data) => {
     
 }
 
-// const handleComments = (data)=> {
-//     const comments = imageCard.querySelector("#comments-list");
+const handleComments = (data)=> {
+    const comments = imageCard.querySelector("#comments-list");
+    comments.innerHTML = '';
 
-//     console.log(data);
-// }
+    data.forEach(comment => {
+        let listElement = document.createElement('li');
+        listElement.textContent= comment.content
+        
+        // console.log(comment.content);
+        comments.appendChild(listElement)
+    })
+}
 
 document.addEventListener('DOMContentLoaded', init)
