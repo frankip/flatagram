1334c9b89af6f44e2a114d23a6372221c819fe09
Francis <gwadafrank@gmail.com>
2022-09-07 19:41:14 +0300

    fetch data from the api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+const baseUrl = "http://localhost:3000";
+const init = () => {
+    console.log("we are");
+
+    fetchDog().catch(e => console.log(e))
+}
+
+async function fetchDog(){
+    const response = await fetch(`${baseUrl}/images/1`);
+    const data = await response.json()
+
+    console.log(data);
+    
+}
+
+document.addEventListener('DOMContentLoaded', init)
